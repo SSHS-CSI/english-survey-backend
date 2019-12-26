5336237a28107821ad4e324ef8530400a6237b98
Sungbin Jo <pcr910303@icloud.com>
2019-12-26 10:27:46 +0900

    Update API to match other formats

--- src/api/master/active.js
+++ src/api/master/active.js
@@ -8,10 +8,11 @@ module.exports = async (ctx, next) => {
         ctx.error(404, "no-result");
     }
 
-    ctx.body = result.map(data => {
+    ctx.body.data = result.map(data => {
         let processed = {};
         processed.username = data.username;
         processed.type = data.type;
         return processed;
     });
+    ctx.body.status = "success";
 };
