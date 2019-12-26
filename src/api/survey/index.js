008a357282b86b7a506b45da75287f8a4c3b9af7
Sungbin Jo <pcr910303@icloud.com>
2019-12-27 08:42:15 +0900

    Add student-count route

--- src/api/survey/index.js
+++ src/api/survey/index.js
@@ -6,5 +6,9 @@ const survey = new Router();
 
 survey.get("/response", requireAuth, require("./response-get.js"));
 survey.post("/response", requireAuth, require("./response-post.js"));
+survey.get("/student-count", async (ctx) => {
+    ctx.body.data = require("./students.js");
+    ctx.body.status = "success";
+});
 
 module.exports = survey;
