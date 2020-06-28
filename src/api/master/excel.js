fb8f57fd7d3f384304fa463109ba9c3034bc5e1d
james0248 <hsjames0248@gmail.com>
2020-06-28 19:05:13 +0900

    Fix excel form 1 styles to match form 2

--- src/api/master/excel.js
+++ src/api/master/excel.js
@@ -13,7 +13,12 @@ module.exports = async (ctx, next) => {
     let outFile = null;
 
     if (type === 0) {
-        if(!ctx.request.body.username) {
+        if (
+            !ctx.request.body.username ||
+            !ctx.request.body.col ||
+            !Number.isInteger(ctx.request.body.col) ||
+            ctx.request.body.col <= 0
+        ) {
             ctx.error(400, 'form-malformed');
         }
         let username = ctx.request.body.username || ctx.query.username;
@@ -30,8 +35,7 @@ module.exports = async (ctx, next) => {
             });
         });
 
-        ws = getWS1(result.response.data);
-        console.log(ws)
+        ws = getWS1(result.response.data, ctx.request.body.col);
         XLSX.utils.book_append_sheet(wb, ws, username);
         outFile = XLSX.write(wb, { bookType: 'xlsx', type: 'base64' });
     } else if (type === 1) {
@@ -51,21 +55,23 @@ module.exports = async (ctx, next) => {
     ctx.body = new Buffer(outFile, 'base64');
 }
 
-let getWS1 = result => {
+let getWS1 = (result, col) => {
     let ws = {};
+    ws['!merges'] = []
     result.forEach((student, idx1) => {
-        let cellChar = 4 * (idx1 % 5) + 1;
-        let cellNum = Math.floor(idx1 / 5) * (result[0].length + 1) + 1;
-        ws[toExcelChar(cellChar) + cellNum] = { t: 's', v: idx1 + 1 };
-        ws[toExcelChar(cellChar + 1) + cellNum] = { t: 's', v: '전' };
-        ws[toExcelChar(cellChar + 2) + cellNum] = { t: 's', v: '후' };
+        let cellChar = 3 * (idx1 % col) + 1;
+        let cellNum = Math.floor(idx1 / col) * (result[0].length + 3) + 1;
+        ws[toExcelChar(cellChar) + cellNum] = { t: 's', v: 'S' + (idx1 + 1) };
+        ws[toExcelChar(cellChar) + (cellNum + 1)] = { t: 's', v: '전' };
+        ws[toExcelChar(cellChar + 1) + (cellNum + 1)] = { t: 's', v: '후' };
         student.forEach((question, idx2) => {
             let type = (typeof (question.left) === 'string') ? 's' : 'n';
-            ws[toExcelChar(cellChar + 1) + (cellNum + idx2 + 1)] = { t: type, v: question.left };
-            ws[toExcelChar(cellChar + 2) + (cellNum + idx2 + 1)] = { t: type, v: question.right };
+            ws[toExcelChar(cellChar) + (cellNum + idx2 + 2)] = { t: type, v: question.left };
+            ws[toExcelChar(cellChar + 1) + (cellNum + idx2 + 2)] = { t: type, v: question.right };
         })
+        ws['!merges'].push({ s: { r: cellNum - 1, c: cellChar - 1 }, e: { r: cellNum - 1, c: cellChar } })
     })
-    ws['!ref'] = 'A1:S1000'
+    ws['!ref'] = 'A1:ZZ1000'
 
     return ws;
 }
@@ -78,7 +84,7 @@ let getWS2 = result => {
         survey.response.data.forEach((student, idx2) => {
             let cellNum = 2 * (idx1 + 1);
             student.forEach((question, idx3) => {
-                let type = (typeof(question.left) === 'string') ? 's' : 'n';
+                let type = (typeof (question.left) === 'string') ? 's' : 'n';
                 ws[toExcelChar(2 * idx2 + 2) + (cellNum + idx3)] = { t: type, v: question.left };
                 ws[toExcelChar(2 * idx2 + 3) + (cellNum + idx3)] = { t: type, v: question.right };
             })
@@ -86,7 +92,6 @@ let getWS2 = result => {
     })
     for (let i = 0; i < result[0].response.data.length; i++) {
         ws[toExcelChar((i + 1) * 2) + 1] = { t: 's', v: 'S' + (i + 1) };
-        console.log(toExcelChar((i + 1) * 2) + 1)
         ws['!merges'].push({ s: { r: 0, c: i * 2 + 1 }, e: { r: 0, c: i * 2 + 2 } });
     }
     ws['!ref'] = 'A1:ZZ1000'
